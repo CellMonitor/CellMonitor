0cdd60aacf40783a87fe2384601fd0eda3ceb099
Joe <youssifhamdy83@yahoo.com>
2017-12-06 03:52:49 +0200

    2

--- app/src/main/java/com/example/joe/cellmonitor/FirstActivity.java
+++ app/src/main/java/com/example/joe/cellmonitor/FirstActivity.java
@@ -1,13 +1,10 @@
 package com.example.joe.cellmonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -17,6 +14,12 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
 
 
+
+    }
+    public void buttonClicked(View view){
+
+        Intent intent = new Intent(FirstActivity.this,LoginActivity.class);
+        startActivity(intent);
 
     }
 
---
+++ app/src/main/java/com/example/joe/cellmonitor/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.joe.cellmonitor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager ;
+    LoginButton loginButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(LoginActivity.this,"Login Success \n" + loginResult.getAccessToken().getUserId()
+                        + " \n " + loginResult.getAccessToken().getToken(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(LoginActivity.this,"Login Cancelled",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(LoginActivity.this,"Login Error",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
