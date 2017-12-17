01fc4c377322e7195c4ecd5b96458b43775cef40
Joe <youssifhamdy83@yahoo.com>
2017-12-17 11:37:38 +0200

    2

--- app/src/main/java/com/example/joe/cellmonitor/HomeActivity.java
+++ app/src/main/java/com/example/joe/cellmonitor/HomeActivity.java
@@ -1,16 +1,70 @@
 package com.example.joe.cellmonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.FacebookButtonBase;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
 
+    private FirebaseAuth mAuth;
     public static final String USER_ID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_profile) {
+            return true;
+        }
+
+        else if (id == R.id.action_map) {
+            return true;
+        }
+
+        else if (id == R.id.action_logOut) {
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            LoginManager.getInstance().logOut();
+            startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+        }
+
+        else if (id == R.id.action_requests) {
+            return true;
+        }
+
+        else if (id == R.id.action_sections) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
