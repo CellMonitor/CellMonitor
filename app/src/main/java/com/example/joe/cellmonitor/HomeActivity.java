81deffbc8c1732da7e40cbdd9c4746a4a476274c
Joe <youssifhamdy83@yahoo.com>
2017-12-26 17:57:16 +0200

    2

--- app/src/main/java/com/example/joe/cellmonitor/HomeActivity.java
+++ app/src/main/java/com/example/joe/cellmonitor/HomeActivity.java
@@ -1,19 +1,35 @@
 package com.example.joe.cellmonitor;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
 
 
+    private static final String TAG = "HomeActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private FirebaseAuth mAuth;
 
 
@@ -23,9 +39,38 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+
+        ImageView imgProfile = (ImageView) findViewById(R.id.profile_pic);
+        Picasso.with(HomeActivity.this).load(user.getPhotoUrl()).into(imgProfile);
 
 
 
+
+    }
+
+    public boolean isServicesOK(){
+
+        Log.d(TAG,"isServicesOK : checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(HomeActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG , "isServicesOK : Google Play Services is working");
+            return true;
+
+        }
+        else if ( GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured byt we can resolve it
+            Log.d(TAG , "isServicesOK : an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(HomeActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else{
+            Toast.makeText(this , " You Can't make map requests",Toast.LENGTH_SHORT).show();
+
+        }
+        return false;
 
 
     }
@@ -51,7 +96,12 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         else if (id == R.id.action_map) {
-            return true;
+            if (isServicesOK()){
+
+                Intent intent = new Intent(HomeActivity.this,MapActivity.class);
+                startActivity(intent);
+
+            }
         }
 
         else if (id == R.id.action_logOut) {
@@ -93,4 +143,6 @@ public class HomeActivity extends AppCompatActivity {
 
         }
     }
+
+    public void addConnectionButtonClicked (View view){}
 }
