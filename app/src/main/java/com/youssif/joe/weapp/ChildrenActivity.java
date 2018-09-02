327bb1aadc2a858964b1a546ba95e89ed25d3677
Joe <youssifhamdy83@yahoo.com>
2018-09-02 14:21:10 +0200

    4

---
+++ app/src/main/java/com/youssif/joe/weapp/ChildrenActivity.java
@@ -0,0 +1,285 @@
+package com.youssif.joe.weapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.youssif.joe.weapp.models.Friends;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.youssif.joe.weapp.GetTimeAgo.getTimeAgo;
+
+public class ChildrenActivity extends AppCompatActivity {
+
+    private RecyclerView mChildrenList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mFamilyDatabase, mChildrenDatabase;
+    private List<Address> addresses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children);
+        Toolbar mUsersToolbar = findViewById(R.id.children_app_bar);
+        setSupportActionBar(mUsersToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Children ❤");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mChildrenList = findViewById(R.id.childrenList);
+        mAuth = FirebaseAuth.getInstance();
+
+        String mCurrent_user_id = mAuth.getCurrentUser().getUid();
+
+        mFamilyDatabase = FirebaseDatabase.getInstance().getReference().child("Family").child(mCurrent_user_id);
+        mFamilyDatabase.keepSynced(true);
+
+        mChildrenDatabase = FirebaseDatabase.getInstance().getReference().child("Children");
+        mChildrenDatabase.keepSynced(true);
+
+
+        mChildrenList.setHasFixedSize(true);
+        mChildrenList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChildrenActivity.this, ChildrenSearch.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        FirebaseRecyclerOptions<Friends> options = new FirebaseRecyclerOptions.Builder<Friends>()
+                .setQuery(mFamilyDatabase, Friends.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        FirebaseRecyclerAdapter<Friends, MyChildrenViewHolder> friendsRecyclerViewAdapter = new FirebaseRecyclerAdapter<Friends, MyChildrenViewHolder>(options) {
+
+            @Override
+            public MyChildrenViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_single_layout, parent, false);
+
+                return new MyChildrenViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull final MyChildrenViewHolder myChildrenViewHolder, int position, @NonNull Friends friends) {
+
+
+                final String list_user_id = getRef(position).getKey();
+                Log.d("list_user_id : ", list_user_id);
+
+                mChildrenDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d("DataSnapShot2 : ", dataSnapshot.toString());
+
+                        final String userName = dataSnapshot.child("name").getValue().toString();
+                        final String userNumber = dataSnapshot.child("phone_number").getValue().toString();
+                        String userThumb = dataSnapshot.child("thumb_image").getValue().toString();
+                        if (dataSnapshot.hasChild("Location")) {
+                            String location = dataSnapshot.child("Location").getValue().toString();
+                            long locationTime = (long) dataSnapshot.child("Location_Time").getValue();
+
+                            String[] separated = location.split(",");
+                            String latiPos = separated[0].trim();
+                            String longiPos = separated[1].trim();
+
+                            double latitude = Double.parseDouble(latiPos);
+                            double longitude = Double.parseDouble(longiPos);
+
+
+                            Geocoder geocoder;
+
+                            geocoder = new Geocoder(ChildrenActivity.this, Locale.getDefault());
+
+                            try {
+                                addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+
+                            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                            //String city = addresses.get(0).getLocality();
+                            //String state = addresses.get(0).getAdminArea();
+                            //String country = addresses.get(0).getCountryName();
+                            //String postalCode = addresses.get(0).getPostalCode();
+                            //String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+
+                            String lastSeenTime = getTimeAgo(locationTime, ChildrenActivity.this);
+
+                            myChildrenViewHolder.setStatus(address + "\n" + lastSeenTime);
+                        }
+                        myChildrenViewHolder.setName(userName);
+
+                        myChildrenViewHolder.setUserImage(userThumb, ChildrenActivity.this);
+
+                        myChildrenViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                CharSequence options[] = new CharSequence[]{"Show " + userName + " On Map!", "Call " + userName};
+
+                                final AlertDialog.Builder builder = new AlertDialog.Builder(ChildrenActivity.this);
+
+                                builder.setTitle("Select Options");
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        //Click Event for each item.
+                                        if (i == 0) {
+
+                                            Intent intent = new Intent(ChildrenActivity.this, MapActivity.class);
+                                            intent.putExtra("Child_ID", list_user_id);
+                                            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(intent);
+                                            finish();
+
+
+                                        }
+                                        if (i == 1) {
+
+
+                                            String[] permissions = {Manifest.permission.CALL_PHONE};
+
+                                            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + userNumber));
+                                            if (ActivityCompat.checkSelfPermission(ChildrenActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                                // TODO: Consider calling
+                                                //    ActivityCompat#requestPermissions
+                                                // here to request the missing permissions, and then overriding
+                                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                                //                                          int[] grantResults)
+                                                // to handle the case where the user grants the permission. See the documentation
+                                                // for ActivityCompat#requestPermissions for more details.
+                                                ActivityCompat.requestPermissions(ChildrenActivity.this,permissions,1234);
+
+                                                if (ActivityCompat.checkSelfPermission(ChildrenActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                                    return;
+                                                }
+                                            }
+                                            startActivity(intent);
+
+
+                                        }
+
+
+                                    }
+                                });
+
+                                builder.show();
+
+                            }
+                        });
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+            }
+
+
+        };
+        mChildrenList.setAdapter(friendsRecyclerViewAdapter);
+
+
+
+
+    }
+
+    public static class MyChildrenViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        MyChildrenViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+
+
+        public void setName(String name) {
+
+            TextView userNameView = mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+
+        }
+
+        public void setStatus(String status){
+            TextView userStatusView =  mView.findViewById(R.id.user_single_status);
+            userStatusView.setText(status);
+
+        }
+
+        void setUserImage(final String thumb_image, final Context ctx) {
+
+            final CircleImageView userImageView = mView.findViewById(R.id.user_single_image);
+            Picasso.get().load(thumb_image).placeholder(R.drawable.avatar).into(userImageView);
+
+
+        }
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/youssif/joe/weapp/MessageAdapter.java
@@ -0,0 +1,254 @@
+package com.youssif.joe.weapp;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.youssif.joe.weapp.models.Messages;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>{
+
+
+    private List<Messages> mMessageList;
+    private String mChatUser;
+    private Context ctx;
+
+    MessageAdapter(List<Messages> mMessageList, String mChatUser, Context ctx) {
+
+        this.mMessageList = mMessageList;
+        this.mChatUser = mChatUser;
+        this.ctx = ctx;
+
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.message_single_layout ,parent, false);
+
+        return new MessageViewHolder(v);
+
+    }
+
+    class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        TextView messageText;
+        CircleImageView profileImage;
+        TextView displayName;
+        ImageView messageImage;
+        TextView messageTime;
+
+        MessageViewHolder(View view) {
+            super(view);
+
+            messageTime = view.findViewById(R.id.time_text_layout);
+            messageText = view.findViewById(R.id.message_text_layout);
+            profileImage = view.findViewById(R.id.message_profile_layout);
+            displayName = view.findViewById(R.id.name_text_layout);
+            messageImage = view.findViewById(R.id.message_image_layout);
+
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MessageViewHolder viewHolder, int i) {
+
+        final Messages c = mMessageList.get(i);
+
+        String from_user = c.getFrom();
+        String message_type = c.getType();
+        final long message_time = c.getTime();
+        final long timestamp = c.getTime();
+
+
+        DatabaseReference mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                String name = dataSnapshot.child("name").getValue().toString();
+                final String image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                viewHolder.displayName.setText(name);
+
+                Picasso.get().load(image).placeholder(R.drawable.avatar).into(viewHolder.profileImage);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        if(message_type.equals("text")) {
+
+            //Convert timestamp to local device time
+
+            SimpleDateFormat sfd = new SimpleDateFormat("hh:mm a");
+            String time = sfd.format(new Date(message_time));
+
+
+            viewHolder.messageText.setText(c.getMessage());
+            viewHolder.messageImage.setVisibility(View.INVISIBLE);
+            viewHolder.messageTime.setText(time);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        } else {
+
+            SimpleDateFormat sfd = new SimpleDateFormat("hh:mm a");
+            String time = sfd.format(new Date(message_time));
+            viewHolder.messageTime.setText(time);
+
+            viewHolder.messageText.setVisibility(View.INVISIBLE);
+            Picasso.get().load(c.getMessage()).into(viewHolder.messageImage);
+
+
+        }
+
+        // ----------------- DELETE FEATURE --------------------
+
+        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(final View view) {
+                DatabaseReference senderRef = FirebaseDatabase.getInstance().getReference("messages")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(mChatUser);
+
+                DatabaseReference receiverRef = FirebaseDatabase.getInstance().getReference("messages")
+                        .child(mChatUser).child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                final Query receiverQuery = receiverRef.orderByChild("time").equalTo(timestamp);
+
+                final Query senderQuery = senderRef.orderByChild("time").equalTo(timestamp);
+
+                CharSequence options[] = new CharSequence[]{"Delete message"};
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+
+                builder.setTitle("Select Options");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, final int i) {
+
+                        //Click Event for each item.
+                        if(i == 0){
+                            final ProgressDialog progressDialog = new ProgressDialog(ctx);
+                            progressDialog.setMessage("Deleting the message for me ..");
+                            progressDialog.show();
+
+                            senderQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                    if (dataSnapshot.hasChildren()) {
+                                        for (DataSnapshot messageSnapshot : dataSnapshot.getChildren()) {
+                                            messageSnapshot.getRef().removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(ctx, "Message Deleted !", Toast.LENGTH_SHORT).show();
+                                                    int position = viewHolder.getAdapterPosition();
+                                                    mMessageList.remove(position);
+                                                    notifyItemRemoved(position);
+                                                    notifyItemRangeChanged(position,mMessageList.size());
+                                                    progressDialog.dismiss();
+
+                                                }
+                                            });
+
+                                        }
+                                    }
+                                    else {
+                                        Toast.makeText(ctx, "Please Rejoin the chat room first", Toast.LENGTH_SHORT).show();
+                                        progressDialog.dismiss();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+/*
+                            receiverQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                                        messageSnapshot.getRef().removeValue();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+*/
+
+                        }
+
+
+                    }
+                });
+
+                builder.show();
+
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+
+
+
+
+
+}
